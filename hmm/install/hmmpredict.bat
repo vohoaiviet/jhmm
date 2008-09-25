fd1f9d5153c1957518a086729d7fb7756c77ee3f
alexmilowski <alexmilowski@097c2fe4-8b3d-11dd-84d8-5ff37064ad4b>
2008-09-25 21:12:49 +0000

    update install build procedure

--- hmm/install/hmmpredict.bat
+++ hmm/install/hmmpredict.bat
@@ -1,1 +1,1 @@
-java -cp $INSTALL_PATH/lib/antlr.jar;$INSTALL_PATH/lib/jhmm.jar -Xmx2048m com.milowski.hmm.tools.Predict %1 %2 %3 %4 %5 %6 %7 %8 %9
+java -cp $INSTALL_PATH/jhmm.jar -Xmx2048m com.milowski.hmm.tools.Predict %1 %2 %3 %4 %5 %6 %7 %8 %9
